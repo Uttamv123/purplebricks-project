e8a73a4191b19e4dfb38e3f0a62e36688e8d4e51
Uttamv123 <uttam@thecodereflections.com>
2026-05-28 22:51:49 +0530

    Move Netlify function and config inside frontend

---
+++ frontend/netlify/functions/getProperties.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-southeast-2',
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+});
+
+exports.handler = async () => {
+  try {
+    const params = {
+      TableName: 'properties'
+    };
+
+    const data = await dynamo.scan(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
